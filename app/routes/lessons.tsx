90b218ac74d41b6e3e0999253b0eb29a6636fdd7
AmitYoffe <yoffe.amit@gmail.com>
2024-01-08 10:45:09 +0200

    test123

--- app/routes/lessons.tsx
+++ app/routes/lessons.tsx
@@ -4,26 +4,43 @@ import FullFeaturedCrudGrid from "~/LessonComponents/DataGrid";
 import { getTopics } from "~/utils/topic.server";
 
 export async function loader() {
-    const topics = await getTopics();
-    const topicWithLessonsCount = topics.map((topic) => { return { ...topic, _count: topic._count.Lessons } })
+  const topics = await getTopics();
+  const topicWithLessonsCount = topics.map((topic) => {
+    return { ...topic, _count: topic._count.Lessons };
+  });
 
-    return { topics: topicWithLessonsCount }
+  return { topics: topicWithLessonsCount };
 }
 
 export default function TopicList() {
-    const { topics } = useLoaderData<typeof loader>();
+  const { topics } = useLoaderData<typeof loader>();
 
-    const columns: GridColDef[] = [
-        { field: 'name', headerName: 'שם נושא', width: 180, editable: true },
-        { field: 'updatedAt', headerName: 'נערך לאחרונה', width: 180, editable: false },
-        { field: '_count', headerName: 'מספר שיעורים', width: 180, editable: false },
-    ]
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "שם נושא", width: 180, editable: true },
+    {
+      field: "updatedAt",
+      headerName: "נערך לאחרונה",
+      width: 180,
+      editable: false,
+    },
+    {
+      field: "_count",
+      headerName: "מספר שיעורים",
+      width: 180,
+      editable: false,
+    },
+  ];
 
-    return (
-        <>
-            <div>
-                <FullFeaturedCrudGrid editable rows={topics} setRows={(oldRows) => { }} columnProps={columns} />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <FullFeaturedCrudGrid
+          editable
+          rows={topics}
+          setRows={(oldRows) => {}}
+          columnProps={columns}
+        />
+      </div>
+    </>
+  );
 }
